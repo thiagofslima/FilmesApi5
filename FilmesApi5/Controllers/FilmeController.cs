62a62d48dc3269cfe76424641a615043cf8046dd
Thiago Lima <thiagofslima@gmail.com>
2024-02-19 23:41:33 -0300

    Aula 02

---
+++ FilmesApi5/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApi5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesApi5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
